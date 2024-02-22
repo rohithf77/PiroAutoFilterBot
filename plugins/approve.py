3fc760fca4e5094a61726f427a545643d7f82e97
rohithf77 <156700198+rohithf77@users.noreply.github.com>
2024-02-22 11:10:05 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -7,7 +7,7 @@ from pyrogram.errors import UserIsBlocked, PeerIdInvalid
 async def accept_request(client, r):
 
     rm = InlineKeyboardMarkup([[
-        InlineKeyboardButton("❤️‍🔥 𝖡𝖫𝖠𝖲𝖳𝖤𝖱 𝖧𝖴𝖡 ❤️‍🔥", url=f"https://t.me/f7moviesofficial"),
+        InlineKeyboardButton("❤️‍🔥 F7 OFFICIAL ❤️‍🔥", url=f"https://t.me/f7moviesofficial"),
         InlineKeyboardButton("⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡", url=f"https://t.me/f7xbots")
     ]])
     
